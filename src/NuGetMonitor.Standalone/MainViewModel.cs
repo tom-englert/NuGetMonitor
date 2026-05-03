9cd699756265f5c547bf0185497628f3ee8bde7b
tom-englert <mail@tom-englert.de>
2026-05-03 08:23:57 +0200

    Standalone: Add logging and info bar

--- src/NuGetMonitor.Standalone/MainViewModel.cs
+++ src/NuGetMonitor.Standalone/MainViewModel.cs
@@ -1,12 +1,17 @@
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
+using NuGetMonitor.Model.Services;
+using NuGetMonitor.Services;
 using NuGetMonitor.View.Monitor;
 
 namespace NuGetMonitor;
 
 internal sealed partial class MainViewModel : INotifyPropertyChanged
 {
-    public bool IsLoading { get; private set; }
+    private readonly LoggerSink _loggerSink;
+    private readonly InfoBarService _infoBarService;
+    private readonly MonitorService _monitorService;
 
     public string? SolutionPath { get; private set; }
 
@@ -14,9 +19,24 @@ internal sealed partial class MainViewModel : INotifyPropertyChanged
 
     public object? NuGetMonitorViewModel { get; } = new NuGetMonitorViewModel();
 
+    public ReadOnlyObservableCollection<LogEntry> LogEntries => _loggerSink.LogEntries;
+
+    public ReadOnlyObservableCollection<InfoBarMessage> InfoBarMessages => _infoBarService.Messages;
+
     public ICommand BrowseCommand => new DelegateCommand(() => BrowseRequested?.Invoke(this, EventArgs.Empty));
 
     internal EventHandler? BrowseRequested;
+
+    public MainViewModel()
+    {
+        _loggerSink = new LoggerSink();
+        LoggerService.AddSink(_loggerSink);
+
+        _infoBarService = new InfoBarService();
+
+        _monitorService = new MonitorService(_infoBarService);
+        _monitorService.RegisterEventHandlers();
+    }
 
     internal async Task LoadSolutionAsync(string? path)
     {
@@ -26,5 +46,10 @@ internal sealed partial class MainViewModel : INotifyPropertyChanged
         PlatformAbstractions.OpenSolution(path);
 
         await Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _monitorService.UnregisterEventHandlers();
     }
 }
